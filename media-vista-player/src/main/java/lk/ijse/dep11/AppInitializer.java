e12628b5ccc1e48feabb16ce025e32e7fbdde40e
Nilan Dinushka <nilandinushka5@gmail.com>
2023-08-29 23:31:04 +0530

    designed layout using scene builder

--- media-vista-player/src/main/java/lk/ijse/dep11/AppInitializer.java
+++ media-vista-player/src/main/java/lk/ijse/dep11/AppInitializer.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class AppInitializer extends Application {
@@ -14,7 +15,7 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        AnchorPane root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
+        BorderPane root = FXMLLoader.load(getClass().getResource("/view/MainView.fxml"));
         Scene mainScene = new Scene(root);
         primaryStage.setScene(mainScene);
         primaryStage.setTitle("MV Media Player");
--- media-vista-player/src/main/java/lk/ijse/dep11/MainViewController.java
+++ media-vista-player/src/main/java/lk/ijse/dep11/MainViewController.java
@@ -1,4 +1,45 @@
 package lk.ijse.dep11;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+
 public class MainViewController {
+    public BorderPane root;
+    public Button btnOpen;
+    public Button btnPlay;
+    public Button btnStop;
+    public Button btnPause;
+    public Button btnFastFastBackward;
+    public Button btnFastBackward;
+    public Button btnFastForward;
+    public Button btnFastFastForward;
+    public Button btnExit;
+
+    public void btnOpenOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPlayOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnStopOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnPauseOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFastFastBackwardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFastBackwardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFastForwardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnFastFastForwardOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnExitOnAction(ActionEvent actionEvent) {
+    }
 }
